e8e69383eb43ebba1d61b82dcf8dd686ea57879b
Vishal Raman <ramanvishal29@gmail.com>
2026-01-29 02:36:48 +0530

    Update API key in CoinContext.jsx
    Replaced the demo API key with a placeholder for security.

--- src/context/CoinContext.jsx
+++ src/context/CoinContext.jsx
@@ -14,7 +14,7 @@ const CoinContextProvider = (props) => {
       method: "GET",
       headers: {
         accept: "application/json",
-        "x-cg-demo-api-key": "CG-XhjwEmozVYWPnAHq8LQpbZuS"
+        "ADD API KEY"
       }
     };
 
